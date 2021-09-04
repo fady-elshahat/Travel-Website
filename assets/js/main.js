b6431a7b524a68b9c13896068902b5f99cd7f360
fady-elshahat <fady.elshahat@gmail.com>
2021-09-04 18:23:38 +0200

    Modified main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,20 +112,20 @@ window.addEventListener("scroll" , ()=>{
 })
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive(){
      const scrollY = window.pageYOffset
 
      sections.forEach(current =>{
-          const sectionHeight = current.offsetHeight
+          const sectionHeight = current.offsetHeight;
           const sectionTop = current.offsetTop - 50;
-          sectionId = current.getAttribute('id')
 
+          const sectionId = current.getAttribute('id');
           if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-               document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+               document.querySelector(`.nav-menu a[href="#${sectionId}" ]`).classList.add('active-link')
           }else{
-               document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+               document.querySelector(`.nav-menu a[href="#${sectionId}" ]`).classList.remove('active-link')
           }
      })
 }
